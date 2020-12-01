7257ed6a2cd33302bae2f7e94832e2345e00c0ac
giancarlo zuliani <slaigox@gmail.com>
2020-12-01 14:22:08 +0100

    filter added

--- js/script.js
+++ js/script.js
@@ -1,12 +1,24 @@
 const APP = new Vue({
   el:"#root",
   data:{
-    albums: []
+    albums: [],
+    genreselected : "all"
   },
   mounted : function(){
     axios.get("https://flynn.boolean.careers/exercises/api/array/music")
     .then(response => {
       this.albums = response.data.response;
     })
+  },
+  methods:{
+    genreselection(){
+      axios.get("https://flynn.boolean.careers/exercises/api/array/music")
+      .then(response => {
+        this.albums = response.data.response;
+        if(this.genreselected !== "all"){
+          this.albums = this.albums.filter(item => item.genre.toLowerCase() === this.genreselected);
+        }
+      })
+    }
   }
 })
